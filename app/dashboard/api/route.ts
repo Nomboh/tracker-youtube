b48456b37cdbd78552ecf060d98c4fe8ccc3051f
Nomboh Quentin <nombohq14@gmail.com>
2024-02-07 16:19:25 -0800

    changed dot env postgres urls

--- app/dashboard/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/app/lib/prisma";
-
-export async function GET() {
-  const users = await prisma.user.findMany();
-  return Response.json(users);
-}
-
-// http://localhost:3000/api/dashboard
